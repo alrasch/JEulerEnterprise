ef1ace8ff380bf8fe9b9ebf5c6df285be86b1b22
aleksalr <arasch855@gmail.com>
2016-12-03 23:20:40 +0100

    TESTCASE - Add unit test for fibonacci sequence.

---
+++ tests/math/sequences/FibonacciTest.groovy
@@ -0,0 +1,23 @@
+package math.sequences
+
+import org.junit.Test
+
+class FibonacciTest extends GroovyTestCase {
+
+    @Test
+    void testGetSequenceUpTo() {
+        Fibonacci fibonacci = new Fibonacci()
+
+        int[] fib3 = fibonacci.getSequenceUpTo(3).toArray() as int[]
+        int[] fib3Expected = [1, 2, 3] as int[]
+        for (int i = 0; i < fib3.length; i++) {
+            assertEquals(fib3[i], fib3Expected[i])
+        }
+
+        int[] fib10 = fibonacci.getSequenceUpTo(10).toArray() as int[]
+        int[] fib10Expected = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89] as int[]
+        for (int i = 0; i < fib10.length; i++) {
+            assertEquals(fib10[i], fib10Expected[i])
+        }
+    }
+}
