5cd1701b60dc3f64921173f7a54674db49cebdd8
aleksalr <arasch855@gmail.com>
2016-12-04 01:17:10 +0100

    FEATURE - Add fibonacci method for getting all terms lower than n.

--- src/math/sequences/Fibonacci.java
+++ src/math/sequences/Fibonacci.java
@@ -10,6 +10,12 @@ public class Fibonacci {
         this.fibonacci = new ArrayList<Integer>();
     }
 
+    private void init () {
+        if (this.fibonacci.size() == 0) {
+            this.generateNext();
+        }
+    }
+
     /**
      * @param n - How many terms
      * @return - First n terms of the Fibonacci sequence
@@ -19,6 +25,30 @@ public class Fibonacci {
             this.generateNext();
         }
         return this.fibonacci;
+    }
+
+    /**
+     * Returns the Fibonacci sequence up to a maximum term size
+     * @param n - Max size of last element in sequence
+     * @return - Fibonacci terms up to n
+     */
+    public ArrayList<Integer> getTermsLowerThan(int n) {
+        this.init();
+        while (this.fibonacci.get(this.fibonacci.size()-1) < n) {
+            this.generateNext();
+        }
+        if (this.isLastElementGreaterThan(n)) {
+            this.removeLastElement();
+        }
+        return this.fibonacci;
+    }
+
+    private boolean isLastElementGreaterThan (int n) {
+        return this.fibonacci.get(this.fibonacci.size() - 1) > n;
+    }
+
+    private void removeLastElement () {
+        this.fibonacci.remove(this.fibonacci.size() - 1);
     }
 
     /**
