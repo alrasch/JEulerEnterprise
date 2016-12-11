e72768d159c72ced4466628125802df52076cc57
aleksalr <arasch855@gmail.com>
2016-12-11 03:13:01 +0100

    CLEANUP - Auto-format.

--- src/math/prime/Prime.java
+++ src/math/prime/Prime.java
@@ -21,7 +21,7 @@ public class Prime {
             return true;
         }
 
-        for (int i = 3; i <= n/2; i += 2) {
+        for (int i = 3; i <= n / 2; i += 2) {
             if (n % i == 0) {
                 return false;
             }
