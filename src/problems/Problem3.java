35a05cb29de8d3869dd470be7c462cc8186e29b9
aleksalr <arasch855@gmail.com>
2016-12-11 03:16:43 +0100

    FEATURE - Solve problem 3.

---
+++ src/problems/Problem3.java
@@ -0,0 +1,22 @@
+package problems;
+
+import math.prime.Prime;
+
+public class Problem3 {
+    public static void main(String[] args) {
+        Problem3 problem = new Problem3();
+        problem.solve();
+    }
+
+    private void solve() {
+        Prime prime = new Prime();
+
+        long solution = -1;
+        try {
+            solution = prime.getLargestPrimeFactor(600851475143L);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(solution);
+    }
+}
