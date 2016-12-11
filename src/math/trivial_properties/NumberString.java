5a0c40051f3519ee97d53e1a01537e63872aa094
aleksalr <arasch855@gmail.com>
2016-12-11 03:26:06 +0100

    WIP - Initial work on palindromic number check.

---
+++ src/math/trivial_properties/NumberString.java
@@ -0,0 +1,13 @@
+package math.trivial_properties;
+
+public class NumberString {
+
+    public NumberString() {
+    }
+
+    public boolean isPalindrome(long n) {
+        String number = "" + n;
+        int length = number.length();
+        return true;
+    }
+}
